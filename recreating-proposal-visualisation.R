fc9224f7e4e571c7ea312e4accd51ab75a892bc7
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-05-21 18:29:21 +0100

    finding just the AEDs that intersect london

--- recreating-proposal-visualisation.R
+++ recreating-proposal-visualisation.R
@@ -6,10 +6,9 @@ library(spData) # has the ldn dataset
 # Reading the greater london boundary map file 
 lnd_boundary_map <- read_sf("maps/gla")
 plot(st_geometry(lnd_boundary_map),border="darkgray")
+# testing something - to make them have the same crs by overriding 
+lnd_boundary_map = st_transform(lnd_boundary_map, crs=4326)
 
-# Alternative method
-london_border <- read_sf(dsn = "maps/London_GLA_Boundary.shp")
-plot(london_border$geometry)
 
 # Getting the borough maps
 data(lnd) # loads the specified London map dataset
@@ -17,7 +16,15 @@ summary(lnd)
 ldn_boroughs <- st_geometry(lnd) # gets geometry from sf object
 plot(ldn_boroughs)
 
+
 # Importing the AED data
 library(readxl)
-AED_data_20_05_26 <- read_excel("data/AED_data_20-05-26.xlsx", 
+AED_data <- read_excel("data/AED_data_20-05-26.xlsx", 
                                 sheet = "data_extract_2026-05-06")
+# changing lat to be a numeric
+AED_data = transform(AED_data, lat = as.numeric(lat))
+AED_data_sf = st_as_sf(AED_data, coords = c("long", "lat"), crs=st_crs(lnd_boundary_map))
+
+# Finding the AED coordinates that intersect london
+london_idx <- st_intersects(lnd_boundary_map$geometry, AED_data_sf)[[1]]
+ldn_AED_data_sf <-AED_data_sf[london_idx,]
