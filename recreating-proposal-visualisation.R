36deac5eff6ce4eb06c7be3fc9942cf77d0a1436
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-05-21 17:37:11 +0100

    gets the borough map

--- recreating-proposal-visualisation.R
+++ recreating-proposal-visualisation.R
@@ -1,7 +1,18 @@
-require(sf)
+library(sf)
 library(ggplot2)
 library(ggmap)
+library(spData) # has the ldn dataset
 
 # Reading the greater london boundary map file 
 lnd_boundary_map <- read_sf("maps/gla")
 plot(st_geometry(lnd_boundary_map),border="darkgray")
+
+# Alternative method
+london_border <- read_sf(dsn = "maps/London_GLA_Boundary.shp")
+plot(london_border$geometry)
+
+# Getting the borough maps
+data(lnd) # loads the specified London map dataset
+summary(lnd)
+ldn_boroughs <- st_geometry(lnd) # gets geometry from sf object
+plot(ldn_boroughs)
