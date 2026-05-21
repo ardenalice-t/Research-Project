605069f0a8804c77139bd1646099cad53f70471d
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-05-21 18:49:37 +0100

    plotting AEDs in London

--- recreating-proposal-visualisation.R
+++ recreating-proposal-visualisation.R
@@ -3,7 +3,8 @@ library(ggplot2)
 library(ggmap)
 library(spData) # has the ldn dataset
 
-# Reading the greater london boundary map file 
+
+# Reading the greater London boundary map file 
 lnd_boundary_map <- read_sf("maps/gla")
 plot(st_geometry(lnd_boundary_map),border="darkgray")
 # testing something - to make them have the same crs by overriding 
@@ -20,11 +21,22 @@ plot(ldn_boroughs)
 # Importing the AED data
 library(readxl)
 AED_data <- read_excel("data/AED_data_20-05-26.xlsx", 
-                                sheet = "data_extract_2026-05-06")
+                       sheet = "data_extract_2026-05-06")
 # changing lat to be a numeric
 AED_data = transform(AED_data, lat = as.numeric(lat))
-AED_data_sf = st_as_sf(AED_data, coords = c("long", "lat"), crs=st_crs(lnd_boundary_map))
+AED_data_sf = st_as_sf(AED_data, coords = c("long", "lat"), 
+                       crs=st_crs(lnd_boundary_map))
 
-# Finding the AED coordinates that intersect london
+
+# Finding the AED coordinates that intersect London
 london_idx <- st_intersects(lnd_boundary_map$geometry, AED_data_sf)[[1]]
 ldn_AED_data_sf <-AED_data_sf[london_idx,]
+
+
+# Plotting London AEDs
+ggplot() + # initialises a ggplot object, layers added
+  geom_sf(data = ldn_boroughs) +
+  geom_sf(data = ldn_AED_data_sf,
+          size = 0.005,
+          colour = 'red')
+
