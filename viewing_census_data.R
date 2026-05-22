bddbe525214970c95257e1c45158c4cc65efea37
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-05-22 12:02:48 +0100

    fixed filtering to london LSOA

--- viewing_census_data.R
+++ viewing_census_data.R
@@ -2,32 +2,29 @@ library(sf)
 library(ggplot2)
 library(ggmap)
 library(spData) # has the ldn dataset
+library(readr)
+library(dplyr)
 
-# Reading the greater London boundary map file 
-lnd_boundary_map <- read_sf("maps/gla")
-plot(st_geometry(lnd_boundary_map),border="darkgray")
-#lnd_boundary_map = st_transform(lnd_boundary_map, crs=4283)
+ldn_boroughs = c('Barking and Dagenham','Barnet','Bexley','Brent',
+                'Bromley','Camden','City of London','Croydon','Ealing',
+                'Enfield','Greenwich','Hackney','Hammersmith and Fulham',
+                'Haringey','Harrow','Havering','Hillingdon','Hounslow',
+                'Islington','Kensington and Chelsea','Kingston upon Thames',
+                'Lambeth','Lewisham','Merton','Newham','Redbridge',
+                'Richmond upon Thames','Southwark','Sutton','Tower Hamlets',
+                'Waltham Forest','Wandsworth','Westminster')
 
 # Reading the Output_Areas map file 
 lsoa_map <- read_sf("maps/Lower_layer_Super_Output_Areas")
-#oa_map <- read_sf("maps/Output_Areas")
-lnd_lsoa_map <- st_intersection(lnd_boundary_map, lsoa_map)
-test = st_contains(lnd_boundary_map, lsoa_map)[[1]]
-lnd_lsoa_map_contains <- lsoa_map[st_contains(lnd_boundary_map, lsoa_map)[[1]],]
-lnd_lsoa_map_intersects <- lsoa_map[st_intersects(lnd_boundary_map, lsoa_map)[[1]],]
 
-plot(st_geometry(lsoa_areas_map),border="darkgray")
+# Reading a lookup file to get just London LSOA
+LSOA_to_region_lookup <- read_csv("maps/LSOA_to_region_lookup.csv", show_col_types = FALSE)
+ldn_LSOA_region_lookup <- LSOA_to_region_lookup[LSOA_to_region_lookup$RGN22NM == "London",]
 
+# Filtering to london LSOA
+ldn_LSOA_map <- lsoa_map %>% filter(LSOA21NM %in% ldn_LSOA_region_lookup$LSOA21NM)
+
+# Plotting LSOA
 ggplot() + # initializes a ggplot object, layers added
-  geom_sf(data = lnd_lsoa_map)
+  geom_sf(data = ldn_LSOA_map)
 
-ggplot() + # initializes a ggplot object, layers added
-  geom_sf(data = lnd_boundary_map) +
-  geom_sf(data = lnd_lsoa_map_contains)
-
-ggplot() + # initializes a ggplot object, layers added
-  geom_sf(data = lnd_boundary_map) +
-  geom_sf(data = lnd_lsoa_map_contains)
-
-ggplot() + # initializes a ggplot object, layers added
-  geom_sf(data = lnd_boundary_map)
