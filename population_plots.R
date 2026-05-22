5af5b82a3a2792b82fe2b837f5230a1bf8a3777d
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-05-22 14:26:17 +0100

    added age proportion plots

--- population_plots.R
+++ population_plots.R
@@ -12,30 +12,48 @@ library(readr)
 OA_pop_data <- read_csv("data/Age-GH-Sex_Output-Area.csv")
 LSOA_pop_data <- read_csv("data/Sex-GH-Age_LSOA.csv")
 
-# Getting just the data on sex for each area 
-OA_sex_data <- OA_pop_data %>% 
+# Getting just the data for each area 
+OA_observations <- OA_pop_data %>% 
   group_by(`Output Areas Code`)%>% 
   summarise(female_proportion = 
               sum(Observation[`Sex (2 categories) Code`==1]) /
-              sum(Observation))
+              sum(Observation),
+            over_50_proportion = 
+              sum(Observation[(`Age (6 categories) Code`==5) | 
+                                (`Age (6 categories) Code`==6)]) /
+              sum(Observation)
+            )
 
-LSOA_sex_data <- LSOA_pop_data %>% 
+LSOA_observations <- LSOA_pop_data %>% 
   group_by(`Lower layer Super Output Areas Code`)%>% 
   summarise(female_proportion = 
               sum(Observation[`Sex (2 categories) Code`==1]) /
+              sum(Observation),
+            over_50_proportion = 
+              sum(Observation[(`Age (6 categories) Code`==5) | 
+                                (`Age (6 categories) Code`==6)]) /
               sum(Observation))
 
-OA_sex_map <- left_join(ldn_oa_map, OA_sex_data, 
+# Joining the data with the previous maps 
+OA_map <- left_join(ldn_oa_map, OA_observations, 
                         by = c("OA21CD" = "Output Areas Code"))
 
-LSOA_sex_map <- left_join(ldn_lsoa_map, LSOA_sex_data, 
+LSOA_map <- left_join(ldn_lsoa_map, LSOA_observations, 
                         by = c("LSOA21CD" = "Lower layer Super Output Areas Code"))
 
+
+# Plotting the maps showing the gender split 
 ggplot() +
-  geom_sf(data = OA_sex_map, aes(fill = female_proportion))
+  geom_sf(data = OA_map, lwd=0.001, 
+          aes(fill = female_proportion))
 
 ggplot() +
-  geom_sf(data = LSOA_sex_map, aes(fill = female_proportion))
+  geom_sf(data = LSOA_map,lwd=0.001, 
+          aes(fill = female_proportion))
+
+ggplot() +
+  geom_sf(data = LSOA_map, lwd=0.001, 
+          aes(fill = over_50_proportion))
 
           
                 
