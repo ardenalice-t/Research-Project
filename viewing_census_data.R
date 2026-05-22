3b6e3f2a94205550f0aa242ef51317fd5086ab98
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-05-22 02:39:17 +0100

    testing intersecttions

--- viewing_census_data.R
+++ viewing_census_data.R
@@ -12,9 +12,22 @@ plot(st_geometry(lnd_boundary_map),border="darkgray")
 lsoa_map <- read_sf("maps/Lower_layer_Super_Output_Areas")
 #oa_map <- read_sf("maps/Output_Areas")
 lnd_lsoa_map <- st_intersection(lnd_boundary_map, lsoa_map)
+test = st_contains(lnd_boundary_map, lsoa_map)[[1]]
+lnd_lsoa_map_contains <- lsoa_map[st_contains(lnd_boundary_map, lsoa_map)[[1]],]
+lnd_lsoa_map_intersects <- lsoa_map[st_intersects(lnd_boundary_map, lsoa_map)[[1]],]
 
 plot(st_geometry(lsoa_areas_map),border="darkgray")
-output_areas_map = st_transform(output_areas_map, crs=4283)
 
 ggplot() + # initializes a ggplot object, layers added
   geom_sf(data = lnd_lsoa_map)
+
+ggplot() + # initializes a ggplot object, layers added
+  geom_sf(data = lnd_boundary_map) +
+  geom_sf(data = lnd_lsoa_map_contains)
+
+ggplot() + # initializes a ggplot object, layers added
+  geom_sf(data = lnd_boundary_map) +
+  geom_sf(data = lnd_lsoa_map_contains)
+
+ggplot() + # initializes a ggplot object, layers added
+  geom_sf(data = lnd_boundary_map)
