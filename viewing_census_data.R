daf30c19a90d30807916420db94e07e2293cbbcb
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-05-22 12:20:19 +0100

    got it working for OA and saved both files

--- viewing_census_data.R
+++ viewing_census_data.R
@@ -1,7 +1,6 @@
 library(sf)
 library(ggplot2)
 library(ggmap)
-library(spData) # has the ldn dataset
 library(readr)
 library(dplyr)
 
@@ -14,17 +13,25 @@ ldn_boroughs = c('Barking and Dagenham','Barnet','Bexley','Brent',
                 'Richmond upon Thames','Southwark','Sutton','Tower Hamlets',
                 'Waltham Forest','Wandsworth','Westminster')
 
-# Reading the Output_Areas map file 
+# Reading the map files
 lsoa_map <- read_sf("maps/Lower_layer_Super_Output_Areas")
+oa_map <- read_sf("maps/Output_Areas")
 
-# Reading a lookup file to get just London LSOA
+# Reading a look up file to get just London areas
 LSOA_to_region_lookup <- read_csv("maps/LSOA_to_region_lookup.csv", show_col_types = FALSE)
 ldn_LSOA_region_lookup <- LSOA_to_region_lookup[LSOA_to_region_lookup$RGN22NM == "London",]
 
-# Filtering to london LSOA
+# Filtering to London LSOA
 ldn_LSOA_map <- lsoa_map %>% filter(LSOA21NM %in% ldn_LSOA_region_lookup$LSOA21NM)
+ldn_oa_map <- oa_map %>% filter(LSOA21NM %in% ldn_LSOA_region_lookup$LSOA21NM)
 
-# Plotting LSOA
-ggplot() + # initializes a ggplot object, layers added
+# Plotting
+ggplot() + 
   geom_sf(data = ldn_LSOA_map)
+ggplot() +
+  geom_sf(data = ldn_oa_map)
+
+# Saving the filtered maps
+write_sf(ldn_LSOA_map, "maps/LDN_LSOA/ldn_LSOA_map.shp")
+write_sf(ldn_oa_map, "maps/LDN_OA/ldn_oa_map.shp")
 
