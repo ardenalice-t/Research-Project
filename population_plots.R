61668cfff6216c036b2ed0f06cca267e990b80c2
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-05-23 16:46:40 +0100

    household deprivation and workday population

--- population_plots.R
+++ population_plots.R
@@ -6,8 +6,8 @@ library(dplyr)
 library(spdep)
 
 # Reading the map files
-OA_map <- read_sf("maps/LDN_LSOA")
-LSOA_map <- read_sf("maps/LDN_OA")
+OA_map <- read_sf("maps/LDN_OA")
+LSOA_map <- read_sf("maps/LDN_LSOA")
 
 # Reading population data 
 library(readr)
@@ -50,7 +50,7 @@ LSOA_map <- left_join(LSOA_map, LSOA_observations,
                         by = c("LSOA21CD" = "Lower layer Super Output Areas Code"))
 
 
-# Plotting the maps showing the gender split 
+# Plotting the maps 
 ggplot() +
   geom_sf(data = OA_map, lwd=0.001, 
           aes(fill = female_proportion))
@@ -68,25 +68,54 @@ ggplot() +
           aes(fill = bad_gh_proportion))
 
 
-# AED data
+### Workday population density ###
+
+LSOA_WD_data <- read_csv("data/Workday_population_lsoa.csv")
+colnames(LSOA_WD_data)[3] <- "workday_population_density"
+LSOA_map <- left_join(LSOA_map, LSOA_WD_data, 
+                      by = c("LSOA21CD" = "Lower layer Super Output Areas Code"))
+ggplot() +
+  geom_sf(data = LSOA_map, lwd=0.001, 
+          aes(fill = workday_population_density))
+
+
+### Household deprivation ###
+
+LSOA_HD_data <- read_csv("data/household_deprivation_LSOA.csv")
+LSOA_HD_data <- mutate(LSOA_HD_data, `Household deprivation (6 categories) Code` = ifelse(`Household deprivation (6 categories) Code` == -8, NA, `Household deprivation (6 categories) Code`))
+LSOA_HD_data <- LSOA_HD_data %>% 
+  group_by(`Lower layer Super Output Areas Code`)%>% 
+  summarise(avg_deprivation = 
+              sum(Observation * `Household deprivation (6 categories) Code`, na.rm = TRUE) /
+              sum(Observation, na.rm = TRUE))
+LSOA_map <- left_join(LSOA_map, LSOA_HD_data, 
+                      by = c("LSOA21CD" = "Lower layer Super Output Areas Code"))
+ggplot() +
+  geom_sf(data = LSOA_map, lwd=0.001, 
+          aes(fill = avg_deprivation))
+
+
+### AED locations ###
+
 aed_map <- read_sf("data/LDN_AEDs")
 
 # transforming to have the same crs 
-st_crs(aed_map)
 LSOA_map <- st_transform(LSOA_map, crs=st_crs(aed_map))
-st_crs(LSOA_map)
 
-test <-aed_map %>%
-  st_intersection(LSOA_map)
-
+# required to use st_join st_within 
 sf_use_s2(FALSE)
 
-test2 <- st_join(aed_map, LSOA_map, join = st_within)
+# joining the AEDs to the LSOA they are within
+aed_to_LSOA <- st_join(aed_map, LSOA_map, join = st_within)
 
-count_aeds <- count(as_tibble(test2), LSOA21CD, name="count_AEDs")
+# counting AEDs in each LSOA
+count_aeds <- count(as_tibble(aed_to_LSOA), LSOA21CD, name="count_AEDs")
 
+# Plotting resulting map
 LSOA_map <- left_join(LSOA_map, count_aeds, 
                       by = c("LSOA21CD" = "LSOA21CD"))
+# changing any NA to 0
+LSOA_map <- mutate(LSOA_map, "count_AEDs" = ifelse(is.na(count_AEDs), 0, count_AEDs))
 
 ggplot() +
   geom_sf(data = LSOA_map, lwd=0.001, 
