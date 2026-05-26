ddec75ba46ed9c173dca6772b4eedd64084d0d98
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-05-27 00:43:36 +0100

    some testing on offsetting

--- Emulating_lab_2b.R
+++ Emulating_lab_2b.R
@@ -18,16 +18,18 @@ lsoa_nb_lag <- nblag(lsoa_nb, maxlag = 2)
 plot(LSOA_map["ovr_50_"])
 
 colnames(LSOA_map)[13] <- "workday_population_density"
+colnames(LSOA_map)[16] <- "population_density"
 
 A <- nb2listw(poly2nb(LSOA_map), style="B")
-car.out <- spautolm(formula = LSOA_map$cnt_AED~LSOA_map$ovr_50_ + LSOA_map$fml_prp + 
+car.out <- spautolm(formula = LSOA_map$cnt_AED~LSOA_map$ovr_50_ + 
+                      LSOA_map$`2024_p_`+ LSOA_map$fml_prp + 
                       LSOA_map$avg_dpr +  LSOA_map$workday_population_density + 
                       LSOA_map$bd_gh_p, data = LSOA_map, listw=A, family="CAR")
+coef(car.out)
 LSOA_map$F1 <- fitted(car.out)
 plot(LSOA_map["F1"], main="Fitted data from CAR model", lwd=0.001)
 
 LSOA_map$F1R<-fitted(car.out) - LSOA_map$cnt_AED
-coef(car.out)
 plot(LSOA_map["F1R"], main="Residuals")
 
 ggplot() +
@@ -49,7 +51,8 @@ ggplot() +
 
 A2 = nb2listw(nblag_cumul(lsoa_nb_lag),style="B")
 car.out2 <- spautolm(formula = LSOA_map$cnt_AED~LSOA_map$ovr_50_ + LSOA_map$fml_prp + 
-                      LSOA_map$avg_dpr +  LSOA_map$workday_population_density + 
+                       LSOA_map$`2024_p_`+ LSOA_map$avg_dpr +  
+                       LSOA_map$workday_population_density + 
                       LSOA_map$bd_gh_p, data = LSOA_map, listw=A2, family="CAR")
 LSOA_map$F2 <- fitted(car.out2)
 plot(LSOA_map["F2"], main="Fitted data from CAR model", lwd=0.001)
@@ -97,3 +100,20 @@ ggplot() +
   geom_sf(data = LSOA_map, lwd=0.001, 
           aes(fill = cnt_AED)) +
   scale_fill_steps(n.breaks = 8, limits = c(0,3))
+
+
+### TESTING 
+# tryung to set a coef to 0
+coef(car.out2)
+coef(car.out2)[2]
+coef(car.out2)[2] = 0
+class(car.out2)
+
+car.out2 <- spautolm(formula = LSOA_map$cnt_AED~LSOA_map$ovr_50_ + LSOA_map$fml_prp + 
+                       LSOA_map$`2024_p_`+ offset(LSOA_map$avg_dpr) +  
+                       LSOA_map$workday_population_density + 
+                       LSOA_map$bd_gh_p, data = LSOA_map, listw=A2, family="CAR")
+coef(car.out2)
+LSOA_map$F2 <- fitted(car.out2)
+plot(LSOA_map["F2"], main="Fitted data from CAR mowdel", lwd=0.001)
+
