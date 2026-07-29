b04f5d39893304cdacb7441478f3dc217229e784
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-07-29 20:37:43 +0100

    script 1

---
+++ scrips/1_loading_data_to_LSOA_map.R
@@ -0,0 +1,159 @@
+
+# Loading Data to LSOA map ----------------------------------------------------
+
+
+# Set Up ------------------------------------------------------------------
+
+
+## Packages ----------------------------------------------------------------
+
+library(sf)
+library(dplyr) 
+library(readr)
+library(tidyr)
+library(ggplot2)
+
+## Functions ---------------------------------------------------------------
+
+plot_descriptive_ldn <- function(relevant_col, 
+                                 title, legend_title, 
+                                 map = LSOA_map){
+  ggplot() +
+    geom_sf(data = map, lwd=0, 
+            aes(fill = .data[[relevant_col]])) + 
+    scale_fill_continuous(name = legend_title, 
+                          labels = scales::label_number(), 
+                          palette = "viridis") +
+    ggtitle(label = title) +
+    xlab("Longitude") +
+    ylab("Latitude") 
+}
+
+plot_descriptive_ldn_exp_scale <- function(relevant_col, 
+                                 title, legend_title, 
+                                 map = LSOA_map){
+  ggplot() +
+    geom_sf(data = LSOA_map, lwd=0, 
+            aes(fill  = .data[[relevant_col]])) + 
+    scale_fill_continuous(name = legend_title, labels = scales::label_number(), 
+                          palette = "viridis", transform = scales::log10_trans()) +
+    ggtitle(label = title) +
+    xlab("Longitude") +
+    ylab("Latitude")
+}
+
+
+
+## Files -------------------------------------------------------------------
+
+
+
+# Creating London Map -----------------------------------------------------
+
+# Creating London map of LSOAs
+LSOA_map <- read_sf("maps/LDN_LSOA")
+LSOA_map <- select(LSOA_map, -c(LSOA21NMW, BNG_E, BNG_N))
+
+
+# Adding Data -------------------------------------------------------------
+
+## Adding individual data --------------------------------------------------
+
+LSOA_idv_data <- read_csv("data/external_datasets/Sex-GH-Age_LSOA.csv")
+
+# Creating the desired statistics 
+LSOA_idv_statistics <- LSOA_idv_data %>% 
+  group_by(`Lower layer Super Output Areas Code`)%>% 
+  summarise(pc_f = 
+              sum(Observation[`Sex (2 categories) Code`==1]) /
+              sum(Observation),
+            pc_50_plus = 
+              sum(Observation[(`Age (6 categories) Code`==5) | 
+                                (`Age (6 categories) Code`==6)]) /
+              sum(Observation),
+            pc_65_plus = 
+              sum(Observation[ (`Age (6 categories) Code`==6)]) /
+              sum(Observation),
+            pc_bad_gh = 
+              sum(Observation[`General health (4 categories) Code`==3]) /
+              sum(Observation))
+
+# Joining with the LSOA map
+LSOA_map <- left_join(LSOA_map, LSOA_idv_statistics, 
+                      by = c("LSOA21CD" = "Lower layer Super Output Areas Code"))
+
+# Plotting 
+plot_descriptive_ldn(relevant_col = 'pc_50_plus', 
+                     title = "Population Age by LSOA", 
+                     legend_title = "Percent of Population \n50 or above")
+
+
+## Adding Household Data -------------------------------------------
+
+LSOA_hos_data <- read_csv("data/external_datasets/household_deprivation_LSOA.csv")
+
+# 'does not apply' changed to NA 
+LSOA_hos_data <- mutate(LSOA_hos_data, 
+                        `Household deprivation (6 categories) Code` = 
+                          ifelse(`Household deprivation (6 categories) Code` == -8, 
+                                 NA, 
+                                 `Household deprivation (6 categories) Code`))
+
+LSOA_hos_statistics <- LSOA_hos_data %>% 
+  group_by(`Lower layer Super Output Areas Code`)%>% 
+  summarise(avg_hos_depr = 
+              sum(Observation * `Household deprivation (6 categories) Code`, na.rm = TRUE) /
+              sum(Observation, na.rm = TRUE))
+
+# Joining with the LSOA map
+LSOA_map <- left_join(LSOA_map, LSOA_hos_statistics, 
+                      by = c("LSOA21CD" = "Lower layer Super Output Areas Code"))
+
+# Plotting 
+plot_descriptive_ldn(relevant_col = 'avg_hos_depr', 
+                     title = "Household Deprivation by LSOA", 
+                     legend_title = "Average Number of \nDeprivation Dimensions")
+
+## Adding Population Densities ---------------------------------------------
+
+LSOA_popden_data <- read_csv("data/external_datasets/2021 pop density census.csv")
+
+LSOA_popden_statistics <- LSOA_popden_data %>% 
+  group_by(`Lower layer Super Output Areas Code`)%>% 
+  summarise(pop_den = sum(Observation))
+
+# Joining with the LSOA map
+LSOA_map <- left_join(LSOA_map, LSOA_popden_statistics, 
+                      by = c("LSOA21CD" = "Lower layer Super Output Areas Code"))
+
+# Plotting 
+plot_descriptive_ldn(relevant_col = 'pop_den', title = "Population Density by LSOA", 
+                     legend_title = "Residents per Sq Km")
+
+# with exponential scale
+plot_descriptive_ldn_exp_scale(relevant_col = 'pop_den', title = "Population Density by LSOA", 
+                     legend_title = "Residents per Sq Km")
+
+
+LSOA_WD_popden_data <- read_csv("data/external_datasets/WD_pop_den.csv")
+
+LSOA_WD_popden_statistics <- LSOA_WD_popden_data %>% 
+  group_by(`Lower layer Super Output Areas Code`)%>% 
+  summarise(WD_pop_den = sum(`Population Density`))
+
+# Joining with the LSOA map
+LSOA_map <- left_join(LSOA_map, LSOA_WD_popden_statistics, 
+                      by = c("LSOA21CD" = "Lower layer Super Output Areas Code"))
+
+# Plotting 
+plot_descriptive_ldn(relevant_col = 'WD_pop_den', title = "Workday Population Density by LSOA", 
+                     legend_title = "Residents per Sq Km")
+
+# with exponential scale
+plot_descriptive_ldn_exp_scale(relevant_col = 'WD_pop_den', title = "Workday Population Density by LSOA", 
+                     legend_title = "Residents per Sq Km")
+# Saving LSOA Map ---------------------------------------------------------
+
+write_sf(LSOA_map, "data/LSOA_complete_map_July_TEST_CAN_DELETE.gpkg")
+#gpkg file allows for more than 10 character file names 
+       
