6e1b1520a35e57e932d968280de72a3943641a7b
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-07-30 13:45:33 +0100

    removing testing code

--- scrips/4_detrending_deprivation.R
+++ scrips/4_detrending_deprivation.R
@@ -56,22 +56,19 @@ model_10_solution <- spatialreg::spautolm(formula = model_10_formula,
 dpr_coef <- coef(model_10_solution)[["LDN_grid_map$avg_dpr.scaled"]]
 
 LDN_grid_map$demand <- fitted(model_10_solution)
-min(fitted(model_10_solution) == model_10_solution$fit$fitted.values)
 
-LDN_grid_map$demand_detrended <- fitted(model_10_solution) - 
-  (dpr_coef * LDN_grid_map$avg_dpr.scaled)
 LDN_grid_map$AED_count_detrended <- LDN_grid_map$cnt_AED- 
   (dpr_coef * LDN_grid_map$avg_dpr.scaled)
 
 cor(LDN_grid_map$demand,  LDN_grid_map$avg_dpr.scaled)
-cor(LDN_grid_map$demand_detrended,  LDN_grid_map$avg_dpr.scaled)
 cor(LDN_grid_map$AED_count_detrended,  LDN_grid_map$avg_dpr.scaled)
 
 # Plotting result
 plotRegresssion("demand_detrended", "AEDs using CAR model (n=1) DETREND", max_relevant_val = 6,
                 map = LDN_grid_map)
 
-detrend_formula <- LDN_grid_map$demand_detrended ~ 
+
+detrend_formula <- LDN_grid_map$AED_count_detrended ~ 
   LDN_grid_map$pc_f.scaled * LDN_grid_map$pop_den.scaled + 
   LDN_grid_map$pc_50_p.scaled * LDN_grid_map$pop_den.scaled + 
   LDN_grid_map$pc_bd_g.scaled + 
@@ -80,52 +77,20 @@ detrend_formula <- LDN_grid_map$demand_detrended ~
   LDN_grid_map$cnt_spr.scaled * LDN_grid_map$pop_den.scaled + 
   LDN_grid_map$cnt_CHs.scaled
 
-detrend_formula2 <- LDN_grid_map$AED_count_detrended ~ 
-  LDN_grid_map$pc_f.scaled * LDN_grid_map$pop_den.scaled + 
-  LDN_grid_map$pc_50_p.scaled * LDN_grid_map$pop_den.scaled + 
-  LDN_grid_map$pc_bd_g.scaled + 
-  LDN_grid_map$avg_dpr.scaled +
-  LDN_grid_map$WD_pp_d.scaled + 
-  LDN_grid_map$cnt_spr.scaled * LDN_grid_map$pop_den.scaled + 
-  LDN_grid_map$cnt_CHs.scaled
-
-test_formula <-LDN_grid_map$demand ~ 
-  LDN_grid_map$pc_f.scaled * LDN_grid_map$pop_den.scaled + 
-  LDN_grid_map$pc_50_p.scaled * LDN_grid_map$pop_den.scaled + 
-  LDN_grid_map$pc_bd_g.scaled + 
-  LDN_grid_map$avg_dpr.scaled +
-  LDN_grid_map$WD_pp_d.scaled + 
-  LDN_grid_map$cnt_spr.scaled * LDN_grid_map$pop_den.scaled + 
-  LDN_grid_map$cnt_CHs.scaled
 
 # Regression of the demand against deprivation to show there is now no relation, 
 # when accounting for medically relevant variables
+
 detrend_regression_solution <- spatialreg::spautolm(formula = detrend_formula, 
                                                     data = LDN_grid_map, 
                                                     listw = distance1km_A, 
                                                     family = "CAR",
                                                     method = "Matrix_J")
 
-detrend2_regression_solution <- spatialreg::spautolm(formula = detrend_formula2, 
-                                                    data = LDN_grid_map, 
-                                                    listw = distance1km_A, 
-                                                    family = "CAR",
-                                                    method = "Matrix_J")
-
-test_refit_car_solution <- spatialreg::spautolm(formula = test_formula, 
-                                                data = LDN_grid_map, 
-                                                listw = distance1km_A, 
-                                                family = "CAR",
-                                                method = "Matrix_J")
 
 summary(detrend_regression_solution)
-summary(detrend2_regression_solution)
-summary(test_refit_car_solution)
 summary(model_10_solution)
 
-coef(detrend2_regression_solution)
-coef(model_10_solution)
-
-(coef(model_10_solution) - coef(detrend2_regression_solution)) / 
+(coef(model_10_solution) - coef(detrend_regression_solution)) / 
   coef(model_10_solution) 
 
