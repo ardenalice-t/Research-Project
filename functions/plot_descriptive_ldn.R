39607d3c872156d2c89c5c7e02ccbfb23539f13f
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-08-04 12:44:44 +0100

    allowed for capped plot

--- functions/plot_descriptive_ldn.R
+++ functions/plot_descriptive_ldn.R
@@ -20,14 +20,36 @@
 #' 
 plot_descriptive_ldn <- function(relevant_col, 
                                  title, legend_title, 
-                                 map){
-  ggplot() +
-    geom_sf(data = map, lwd=0, 
-            aes(fill = .data[[relevant_col]])) + 
-    scale_fill_continuous(name = legend_title, 
-                          labels = scales::label_number(), 
-                          palette = "viridis") +
-    ggtitle(label = title) +
-    xlab("Longitude") +
-    ylab("Latitude") 
+                                 map, cap=FALSE, max_val=5, bins=6){
+  stopifnot(is.character(relevant_col) &
+              is.character(title) &
+              is.character(legend_title) &
+              is.tbl(map))
+  
+  if(cap){
+    stopifnot(is.numeric(max_val) & is.numeric(bins))
+    final_plot <- ggplot() +
+      geom_sf(data = map, lwd=0, 
+              aes(fill = .data[[relevant_col]])) + 
+      scale_fill_steps(breaks = seq(min(map[[relevant_col]]), max_val, length = bins),
+                       na.value = "light blue",
+                       rescaler = ~ scales::rescale_max(.x, from =c(0,max_val)), 
+                       labels = scales::label_number(0.1),
+                       name = legend_title)  +
+      ggtitle(label = title) +
+      xlab("Longitude") +
+      ylab("Latitude") 
+  }
+  if(!cap){
+    final_plot <- ggplot() +
+      geom_sf(data = map, lwd=0, 
+              aes(fill = .data[[relevant_col]])) + 
+      scale_fill_continuous(name = legend_title, 
+                            labels = scales::label_number(), 
+                            palette = "viridis") +
+      ggtitle(label = title) +
+      xlab("Longitude") +
+      ylab("Latitude") 
+  }
+  final_plot
 }
