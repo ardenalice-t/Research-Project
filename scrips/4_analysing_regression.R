d592e9faddc3e3f53820096125b88fd7110208e2
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-08-06 09:40:56 +0100

    updated to use new fct

--- scrips/4_analysing_regression.R
+++ scrips/4_analysing_regression.R
@@ -1,14 +1,11 @@
 
-# Analysing Regression Results --------------------------------------------
-
-
+# Analyzing Regression Results --------------------------------------------
 
 # Set Up ------------------------------------------------------------------
 
 ## Packages ----------------------------------------------------------------
 
 library(readr)
-library(stringr) #to split strings
 library(ggplot2) # for plotting
 library(dplyr) # for select
 
@@ -20,10 +17,6 @@ read_coef_csv = function(filename){
   import <- read_csv(filename, col_types = cols())
   import <- import[-1]
   
-  model_specs <- do.call(rbind,str_split(import$model,"_A."))
-  import$variable_combination  <- model_specs[,1]
-  import$distance_matrix  <- model_specs[,2]
-  
   ifelse(exists("total_coef_matrix"), 
          total_coef_matrix <<- rbind(total_coef_matrix, import),
          assign("total_coef_matrix", import, envir = .GlobalEnv))
@@ -33,11 +26,11 @@ read_coef_csv = function(filename){
 
 plot_coef_estimates = function(dataframe){
   dataframe %>% 
-    arrange(length(as.character(dataframe$clean_var_name))) %>%
-    mutate(clean_var_name = factor(clean_var_name, levels = unique(clean_var_name))) %>%
+    arrange(length(as.character(dataframe$variable))) %>%
+    mutate(variable = factor(variable, levels = unique(variable))) %>%
     ggplot() +
-    geom_point(aes(clean_var_name, Estimate, 
-                   size = .data[["Pr(>|z|)"]], colour=distance_matrix), 
+    geom_point(aes(variable, Estimate, 
+                   size = .data[["Pr(>|z|)"]], colour=neighbours), 
                alpha=0.3) +
     geom_hline(yintercept = 0, color = "darkgray") + 
     scale_size_continuous(range = c(2,0.01), name = "p value",
@@ -49,12 +42,14 @@ plot_coef_estimates = function(dataframe){
     ylab("Coefficient Estimate") 
 }
 
+source("functions/clean_coef_names.R")
+
 ## Files -------------------------------------------------------------------
 
 # Reading in Coefficients -------------------------------------------------
 
 # Importing saved coefficients
-coef_files <- list.files(path="regression_results/to_import", pattern="*.csv", 
+coef_files <- list.files(path="results/regression_results", pattern="*.csv", 
                          full.names=TRUE, recursive=FALSE)
 
 for(file in coef_files){
@@ -62,43 +57,26 @@ for(file in coef_files){
 }
 
 # Check import finished correctly
-print(length(coef_files) == length(unique(total_coef_matrix$model)))
+print(length(coef_files) == length(unique(total_coef_matrix$formula)) * 
+  length(unique(total_coef_matrix$neighbours))) 
 
 # Cleaning variable names
-total_coef_matrix$clean_var_name = gsub("LDN_grid_map[$]", "", total_coef_matrix$variable)
-total_coef_matrix$clean_var_name = gsub("[.]scaled", "", total_coef_matrix$clean_var_name)
-
-total_coef_matrix$clean_var_name = gsub("[(]Intercept[)]","Intercept", total_coef_matrix$clean_var_name)
-total_coef_matrix$clean_var_name = gsub("pc_f","Female Proportion", total_coef_matrix$clean_var_name)
-total_coef_matrix$clean_var_name = gsub("pc_50_p","50+ Proportion", total_coef_matrix$clean_var_name)
-total_coef_matrix$clean_var_name = gsub("pc_bd_g","Bad General Health Proportion", total_coef_matrix$clean_var_name)
-total_coef_matrix$clean_var_name = gsub("avg_dpr","Average Household Deprivation", total_coef_matrix$clean_var_name)
-total_coef_matrix$clean_var_name = gsub("pop_den","Population Density", total_coef_matrix$clean_var_name)
-total_coef_matrix$clean_var_name = gsub("WD_pp_d","Workday Population Density", total_coef_matrix$clean_var_name)
-total_coef_matrix$clean_var_name = gsub("cnt_spr","Number of Sports Sites", total_coef_matrix$clean_var_name)
-total_coef_matrix$clean_var_name = gsub("cnt_CHs","Number of Care Facilities", total_coef_matrix$clean_var_name)
-total_coef_matrix$clean_var_name = gsub("pc_65_p","65+ Proportion", total_coef_matrix$clean_var_name)
-
-total_coef_matrix$clean_var_name = gsub("Proportion","%", total_coef_matrix$clean_var_name)
-total_coef_matrix$clean_var_name = gsub("Average","Avg.", total_coef_matrix$clean_var_name)
-total_coef_matrix$clean_var_name = gsub("Household","Hos.", total_coef_matrix$clean_var_name)
-total_coef_matrix$clean_var_name = gsub("Population Density","Pop. Den.", total_coef_matrix$clean_var_name)
-total_coef_matrix$clean_var_name = gsub("Number of","#", total_coef_matrix$clean_var_name)
-
-total_coef_matrix$clean_var_name = gsub(":"," x ", total_coef_matrix$clean_var_name)
+total_coef_matrix$variable <- clean_coef_names(total_coef_matrix$variable,
+                                               pc_symbol = TRUE,
+                                               abbreviations = TRUE)
 
 
 # Plotting Regression Coefficients ----------------------------------------
 
 # Organizing according to variable name length
 total_coef_matrix = total_coef_matrix %>% 
-  arrange(length(as.character(total_coef_matrix$clean_var_name))) %>%
-  mutate(clean_var_name = factor(clean_var_name, levels = unique(clean_var_name)))
+  arrange(length(as.character(total_coef_matrix$variable))) %>%
+  mutate(variable = factor(variable, levels = unique(variable)))
 
-larger_coefs = unique(total_coef_matrix$clean_var_name)[1:8]
+larger_coefs = unique(total_coef_matrix$variable)[1:9]
 
-larger_coef_matrix = total_coef_matrix[total_coef_matrix$clean_var_name %in% larger_coefs,]
-smaller_coef_matrix = total_coef_matrix[!total_coef_matrix$clean_var_name %in% larger_coefs,]
+larger_coef_matrix = total_coef_matrix[total_coef_matrix$variable %in% larger_coefs,]
+smaller_coef_matrix = total_coef_matrix[!total_coef_matrix$variable %in% larger_coefs,]
 
 # Plotting results
 plot_coef_estimates(larger_coef_matrix)
@@ -108,7 +86,7 @@ plot_coef_estimates(total_coef_matrix)
 
 # Calculating Correlation -------------------------------------------------
 
-by_model_coefs = total_coef_matrix %>% group_by(model)
+by_model_coefs = total_coef_matrix %>% group_by(formula)
 nModels = max(group_indices(by_model_coefs))
 
 correlations =  data.frame(Model1=character(),
@@ -125,11 +103,11 @@ for(model1_idx in 2:nModels){
     model1_chr = group_keys(by_model_coefs)[model1_idx,][[1]]
     model2_chr = group_keys(by_model_coefs)[model2_idx,][[1]]
     
-    model1 = model1[model1$clean_var_name %in% model2$clean_var_name,]
-    model2 = model2[model2$clean_var_name %in% model1$clean_var_name,]
+    model1 = model1[model1$variable %in% model2$variable,]
+    model2 = model2[model2$variable %in% model1$variable,]
     
-    model1 = model1 %>%  arrange(clean_var_name)
-    model2 = model2 %>%  arrange(clean_var_name)
+    model1 = model1 %>%  arrange(variable)
+    model2 = model2 %>%  arrange(variable)
     
     cor = cor(model1$Estimate, model2$Estimate)
     cor_test <- cor.test(model1$Estimate, model2$Estimate,alternative = "greater")
@@ -154,5 +132,8 @@ print(tibble(correlations[which.min(correlations$Correlation),]))
 
 
 # To Do: Add the AIC plot 
+
+# Realised that the combination rows, and def any squared rows are no longer standardised. 
+# so maybe i have to add them in earlier and standardise them then ? 
 
     
