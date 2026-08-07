77e3e12e49f01380762050fda5535f3265aa60a9
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-08-07 15:12:03 +0100

    cleaning

--- functions/clean_coef_names.R
+++ functions/clean_coef_names.R
@@ -1,9 +1,9 @@
 #' Clean Coefficient Names
 #'
-#' @param column - column of a dataframe containing all the coefficient names 
+#' @param column - column of a dataframe containing all the coefficient names
 #' to clean
 #' @param pc_symbol - whether or not 'proportion' should be displayed as %
-#' @param abbreviations - whether or not additional abbreviations should be 
+#' @param abbreviations - whether or not additional abbreviations should be
 #' applied e.g. average to avg.
 #'
 #' @returns the column with clean names
@@ -12,15 +12,15 @@
 #' @examples
 #' rownames(coef_cor) <- clean_coef_names(rownames(coef_cor))
 clean_coef_names <- function(column, pc_symbol=FALSE, abbreviations=FALSE){
-  stopifnot(is.logical(pc_symbol)) 
+  stopifnot(is.logical(pc_symbol))
   stopifnot(is.logical(abbreviations))
     stopifnot(is.character(column))
-  
+
   # general symbols
   column = gsub("LDN_grid[$]", "", column)
   column = gsub("[.]scaled", "", column)
   column = gsub(":"," x ", column)
-  
+
   # coefficient names
   column = gsub("[(]Intercept[)]","Intercept", column)
   column = gsub("pc_f","Female Proportion", column)
@@ -33,7 +33,7 @@ clean_coef_names <- function(column, pc_symbol=FALSE, abbreviations=FALSE){
   column = gsub("count_sports","Number of Sports Sites", column)
   column = gsub("count_CHs","Number of Care Facilities", column)
   column = gsub("count_AEDss","Number of AEDs", column)
- 
+
   # options
   if(pc_symbol) column = gsub("Proportion","%", column)
   if(abbreviations){
@@ -42,6 +42,6 @@ clean_coef_names <- function(column, pc_symbol=FALSE, abbreviations=FALSE){
     column = gsub("Population Density","Pop. Den.", column)
     column = gsub("Number of","#", column)
   }
-  
+
   return(column)
 }
