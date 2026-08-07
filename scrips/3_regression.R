3bd2d63207ccd735bebcec3c0bae973f0c4c660f
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-08-07 17:03:13 +0100

    shifting

--- scrips/3_regression.R
+++ scrips/3_regression.R
@@ -2,7 +2,6 @@
 
 # Regression --------------------------------------------------------------
 
-
 # Set Up ------------------------------------------------------------------
 
 
@@ -47,7 +46,7 @@ plot(LDN_grid["avg_hos_dpr.scaled"])
 
 # Testing spatial correlation
 
-# queen neighborhood matrix 
+# queen neighborhood matrix
 queen_shared_edge.nb <- poly2nb(LDN_grid,queen=TRUE)
 A.queen_shared_edge <- nb2listw(queen_shared_edge.nb,style="B", zero.policy = TRUE)
 
@@ -58,7 +57,7 @@ moran_I_results <- data.frame(var_name = character(),
 
 for (col in scaled_cols){
   moran_test <- moran.test((LDN_grid[[col]]), A.queen_shared_edge)
-  moran_I_results[nrow(moran_I_results) + 1,]  <- list(col, 
+  moran_I_results[nrow(moran_I_results) + 1,]  <- list(col,
                                                        moran_test$statistic, # not sure if this is supposed to be statistic or estimate
                                                        moran_test$p.value)
 }
@@ -70,7 +69,7 @@ write.csv(moran_I_results, "results/scaled_cols_morans_I.csv")
 # Testing Correlation -----------------------------------------------------
 
 variable_correlation <-cor(st_drop_geometry(LDN_grid[scaled_cols]))
-variable_correlation <- melt(variable_correlation) 
+variable_correlation <- melt(variable_correlation)
 
 ggplot(variable_correlation, aes(X1, X2)) +
   geom_tile(aes(fill = value)) +
@@ -79,17 +78,17 @@ ggplot(variable_correlation, aes(X1, X2)) +
 
 # Linear Test -------------------------------------------------------------
 
-linear_model = LDN_grid$count_AEDs ~ LDN_grid$pc_f.scaled + 
+linear_model = LDN_grid$count_AEDs ~ LDN_grid$pc_f.scaled +
                    LDN_grid$pc_65_plus.scaled +
-                   LDN_grid$pc_50_plus.scaled + 
+                   LDN_grid$pc_50_plus.scaled +
                    LDN_grid$pc_bad_gh.scaled +
                    LDN_grid$avg_hos_dpr.scaled +
-                   LDN_grid$pop_den.scaled+ 
-                   LDN_grid$WD_pop_den.scaled + 
+                   LDN_grid$pop_den.scaled+
+                   LDN_grid$WD_pop_den.scaled +
                    LDN_grid$count_sports.scaled +
                    LDN_grid$count_CHs.scaled
 
-linear.out <- lm(formula = linear_model, 
+linear.out <- lm(formula = linear_model,
                  data = LDN_grid)
 
 linear_summary <- summary(linear.out); linear_summary
@@ -145,203 +144,208 @@ A.lag4 <- nb2listw(nblag_cumul(lag4.nb),style="B", zero.policy = TRUE)
 nearest4.nb <- knn2nb(knearneigh(LDN_grid_centroid,k=4), sym = TRUE)
 A.nearest4 <- nb2listw(nearest4.nb,style="B", zero.policy = TRUE)
 
-A_matrices <- list(A.distance0.5km, A.distance1km, A.distance1.5km, 
+A_matrices <- list(A.distance0.5km, A.distance1km, A.distance1.5km,
                 A.queen_shared_edge, A.rook_shared_edge,
                 A.lag2, A.lag4, A.nearest4)
 
-names(A_matrices) <- list("A.distance0.5km", "A.distance1km", "A.distance1.5km", 
+names(A_matrices) <- list("A.distance0.5km", "A.distance1km", "A.distance1.5km",
                           "A.queen_shared_edge", "A.rook_shared_edge",
                           "A.lag2", "A.lag4", "A.nearest4")
 
 # Visualizing one
 #plot(LDN_grid$geom)
-#plot.nb(distance1km.nb, LDN_grid$geom, add = TRUE, col='red', points=FALSE)  
+#plot.nb(distance1km.nb, LDN_grid$geom, add = TRUE, col='red', points=FALSE)
 
 
 # Models ------------------------------------------------------------------
 
-model_1 <- LDN_grid$count_AEDs ~ 
-  LDN_grid$pc_f.scaled + 
+model_1 <- LDN_grid$count_AEDs ~
+  LDN_grid$pc_f.scaled +
   LDN_grid$pc_65_plus.scaled +
-  LDN_grid$pc_50_plus.scaled + 
+  LDN_grid$pc_50_plus.scaled +
   LDN_grid$pc_bad_gh.scaled +
   LDN_grid$avg_hos_dpr.scaled +
-  LDN_grid$pop_den.scaled+ 
-  LDN_grid$WD_pop_den.scaled + 
+  LDN_grid$pop_den.scaled+
+  LDN_grid$WD_pop_den.scaled +
   LDN_grid$count_sports.scaled +
   LDN_grid$count_CHs.scaled
 
-model_2 <- LDN_grid$count_AEDs ~ 
-  LDN_grid$pc_f.scaled + 
+model_2 <- LDN_grid$count_AEDs ~
+  LDN_grid$pc_f.scaled +
   LDN_grid$pc_65_plus.scaled +
-  #LDN_grid$pc_50_plus.scaled + 
+  #LDN_grid$pc_50_plus.scaled +
   LDN_grid$pc_bad_gh.scaled +
   LDN_grid$avg_hos_dpr.scaled +
-  LDN_grid$pop_den.scaled+ 
-  LDN_grid$WD_pop_den.scaled + 
+  LDN_grid$pop_den.scaled+
+  LDN_grid$WD_pop_den.scaled +
   LDN_grid$count_sports.scaled +
   LDN_grid$count_CHs.scaled
 
-model_3 <- LDN_grid$count_AEDs ~ 
-  LDN_grid$pc_f.scaled + 
+model_3 <- LDN_grid$count_AEDs ~
+  LDN_grid$pc_f.scaled +
   #LDN_grid$pc_65_plus.scaled +
-  LDN_grid$pc_50_plus.scaled + 
+  LDN_grid$pc_50_plus.scaled +
   LDN_grid$pc_bad_gh.scaled +
   LDN_grid$avg_hos_dpr.scaled +
-  LDN_grid$pop_den.scaled+ 
-  LDN_grid$WD_pop_den.scaled + 
+  LDN_grid$pop_den.scaled+
+  LDN_grid$WD_pop_den.scaled +
   LDN_grid$count_sports.scaled +
   LDN_grid$count_CHs.scaled
 
-model_4 <- LDN_grid$count_AEDs ~ 
-  LDN_grid$pc_f.scaled + 
+model_4 <- LDN_grid$count_AEDs ~
+  LDN_grid$pc_f.scaled +
   LDN_grid$pc_65_plus.scaled +
-  LDN_grid$pc_50_plus.scaled + 
+  LDN_grid$pc_50_plus.scaled +
   #LDN_grid$pc_bad_gh.scaled +
   LDN_grid$avg_hos_dpr.scaled +
-  LDN_grid$pop_den.scaled+ 
-  LDN_grid$WD_pop_den.scaled + 
+  LDN_grid$pop_den.scaled+
+  LDN_grid$WD_pop_den.scaled +
   LDN_grid$count_sports.scaled +
   LDN_grid$count_CHs.scaled
 
-model_5 <- LDN_grid$count_AEDs ~ 
-  LDN_grid$pc_f.scaled + 
+model_5 <- LDN_grid$count_AEDs ~
+  LDN_grid$pc_f.scaled +
   LDN_grid$pc_65_plus.scaled +
-  LDN_grid$pc_50_plus.scaled + 
+  LDN_grid$pc_50_plus.scaled +
   LDN_grid$pc_bad_gh.scaled +
   LDN_grid$avg_hos_dpr.scaled +
-  LDN_grid$pop_den.scaled+ 
-  LDN_grid$WD_pop_den.scaled + 
-  LDN_grid$count_sports.scaled 
-  #LDN_grid$count_CHs.scaled 
+  LDN_grid$pop_den.scaled+
+  LDN_grid$WD_pop_den.scaled +
+  LDN_grid$count_sports.scaled
+  #LDN_grid$count_CHs.scaled
 
-model_6 <- LDN_grid$count_AEDs ~ 
-  LDN_grid$pc_f.scaled + 
+model_6 <- LDN_grid$count_AEDs ~
+  LDN_grid$pc_f.scaled +
   LDN_grid$pc_65_plus.scaled +
-  LDN_grid$pc_50_plus.scaled + 
+  LDN_grid$pc_50_plus.scaled +
   LDN_grid$pc_bad_gh.scaled +
   LDN_grid$avg_hos_dpr.scaled +
-  LDN_grid$pop_den.scaled+ 
-  #LDN_grid$WD_pop_den.scaled + 
+  LDN_grid$pop_den.scaled+
+  #LDN_grid$WD_pop_den.scaled +
   LDN_grid$count_sports.scaled +
   LDN_grid$count_CHs.scaled
 
-model_7 <- LDN_grid$count_AEDs ~ 
-  LDN_grid$pc_f.scaled + 
+model_7 <- LDN_grid$count_AEDs ~
+  LDN_grid$pc_f.scaled +
   LDN_grid$pc_65_plus.scaled +
-  LDN_grid$pc_50_plus.scaled + 
+  LDN_grid$pc_50_plus.scaled +
   LDN_grid$pc_bad_gh.scaled +
   LDN_grid$avg_hos_dpr.scaled +
-  LDN_grid$pop_den.scaled+ 
-  LDN_grid$WD_pop_den.scaled + 
+  LDN_grid$pop_den.scaled+
+  LDN_grid$WD_pop_den.scaled +
   LDN_grid$count_sports.scaled +
   LDN_grid$count_CHs.scaled +
-  LDN_grid$pc_f.scaled : LDN_grid$pop_den.scaled + 
+  LDN_grid$pc_f.scaled : LDN_grid$pop_den.scaled +
   LDN_grid$pc_50_plus.scaled : LDN_grid$pop_den.scaled +
-  LDN_grid$pc_65_plus.scaled : LDN_grid$pop_den.scaled + 
+  LDN_grid$pc_65_plus.scaled : LDN_grid$pop_den.scaled +
   LDN_grid$pc_bad_gh.scaled : LDN_grid$pop_den.scaled
 
-model_8 <- LDN_grid$count_AEDs ~ 
-  LDN_grid$pc_f.scaled + 
+model_8 <- LDN_grid$count_AEDs ~
+  LDN_grid$pc_f.scaled +
   LDN_grid$pc_65_plus.scaled +
-  LDN_grid$pc_50_plus.scaled + 
+  LDN_grid$pc_50_plus.scaled +
   LDN_grid$pc_bad_gh.scaled +
   LDN_grid$avg_hos_dpr.scaled +
-  LDN_grid$pop_den.scaled+ 
-  LDN_grid$WD_pop_den.scaled + 
+  LDN_grid$pop_den.scaled+
+  LDN_grid$WD_pop_den.scaled +
   LDN_grid$count_sports.scaled +
   LDN_grid$count_CHs.scaled +
-  LDN_grid$pc_f.scaled : LDN_grid$pop_den.scaled + 
+  LDN_grid$pc_f.scaled : LDN_grid$pop_den.scaled +
   LDN_grid$pc_50_plus.scaled : LDN_grid$pop_den.scaled +
-  LDN_grid$pc_65_plus.scaled : LDN_grid$pop_den.scaled + 
+  LDN_grid$pc_65_plus.scaled : LDN_grid$pop_den.scaled +
   LDN_grid$pc_bad_gh.scaled : LDN_grid$pop_den.scaled +
-  LDN_grid$count_sports.scaled : LDN_grid$pop_den.scaled + 
-  LDN_grid$count_CHs.scaled : LDN_grid$pop_den.scaled 
+  LDN_grid$count_sports.scaled : LDN_grid$pop_den.scaled +
+  LDN_grid$count_CHs.scaled : LDN_grid$pop_den.scaled
 
-model_9 <- LDN_grid$count_AEDs ~ 
-  LDN_grid$pc_f.scaled + 
+model_9 <- LDN_grid$count_AEDs ~
+  LDN_grid$pc_f.scaled +
   LDN_grid$pc_65_plus.scaled +
-  #LDN_grid$pc_50_plus.scaled + 
+  #LDN_grid$pc_50_plus.scaled +
   LDN_grid$pc_bad_gh.scaled +
   LDN_grid$avg_hos_dpr.scaled +
-  LDN_grid$pop_den.scaled+ 
-  LDN_grid$WD_pop_den.scaled + 
+  LDN_grid$pop_den.scaled+
+  LDN_grid$WD_pop_den.scaled +
   LDN_grid$count_sports.scaled +
   LDN_grid$count_CHs.scaled +
-  LDN_grid$pc_f.scaled : LDN_grid$pop_den.scaled + 
+  LDN_grid$pc_f.scaled : LDN_grid$pop_den.scaled +
   LDN_grid$pc_50_plus.scaled : LDN_grid$pop_den.scaled +
   LDN_grid$count_sports.scaled : LDN_grid$pop_den.scaled
 
-model_10 <- LDN_grid$count_AEDs ~ 
-  exp(LDN_grid$pc_f.scaled) + 
-  exp(LDN_grid$pc_65_plus.scaled) +
-  exp(LDN_grid$pc_50_plus.scaled) + 
-  exp(LDN_grid$pc_bad_gh.scaled) +
-  exp(LDN_grid$avg_hos_dpr.scaled) +
-  LDN_grid$pop_den.scaled+ 
-  LDN_grid$WD_pop_den.scaled + 
+# Could make this into a non-linear model
+model_10 <- LDN_grid$count_AEDs ~
+  LDN_grid$pc_f.scaled +
+  LDN_grid$pc_65_plus.scaled +
+  LDN_grid$pc_50_plus.scaled +
+  LDN_grid$pc_bad_gh.scaled +
+  LDN_grid$avg_hos_dpr.scaled +
+  LDN_grid$pop_den.scaled+
+  LDN_grid$WD_pop_den.scaled +
   LDN_grid$count_sports.scaled +
-  LDN_grid$count_CHs.scaled
+  LDN_grid$count_CHs.scaled +
+  LDN_grid$pc_50_plus.scaled : LDN_grid$pc_f.scaled  +
+  LDN_grid$pc_65_plus.scaled : LDN_grid$pc_f.scaled  +
+  LDN_grid$pc_bad_gh.scaled : LDN_grid$pc_f.scaled
 
-models <- list(model_1, model_2, model_3, model_4, model_5, model_6, model_7, 
+models <- list(model_1, model_2, model_3, model_4, model_5, model_6, model_7,
             model_8, model_9, model_10)
 
-names(models) <- c("model_1", "model_2", "model_3", "model_4", "model_5", "model_6", "model_7", 
-                      "model_8", "model_9", "model_10")
+names(models) <- c("model_1", "model_2", "model_3", "model_4", "model_5",
+                   "model_6", "model_7", "model_8", "model_9", "model_10")
 
 
 # Regression --------------------------------------------------------------
 
 # initializing a model ranking csv
-#write.csv(data.frame("Formula", "Neighbor Matrix", "Log Likelihood", "ML Residual Variance", "AIC"), 
-#          "results/regression_results/model_ranking.csv", col.names = FALSE)
+# write.csv(data.frame("Formula", "Neighbor Matrix", "Log Likelihood",
+#                      "ML Residual Variance", "AIC"),
+#           "results/regression_results/model_ranking.csv", col.names = FALSE)
 
 for (n_model in 1:(length(models))){
   model_name <- names(models[n_model])
   model <- models[[model_name]]
-  
+
   print(paste("----", "Starting investigation for:", model_name, "----"))
-  
+
   for(n_Amat in 1:(length(A_matrices))){
     Amat_name <- names(A_matrices[n_Amat])
     Amat <- A_matrices[[Amat_name]]
-    
+
     print(paste("Using neighbourhood matrix:", Amat_name))
-    
+
     # performing regression
-    car_output <- spautolm(formula = model, 
-                        data = LDN_grid, listw=Amat, 
+    car_output <- spautolm(formula = model,
+                        data = LDN_grid, listw=Amat,
                         family="CAR",
                         method = "Matrix_J")
-    
+
     # Creating a dataframe of coefficient data
     unique_model_name = paste(model_name, Amat_name, sep="_")
     filename = paste("results/regression_results/", unique_model_name,
                      ".csv", sep="")
-    
+
     model_summary = summary(car_output)
     coefs = as.data.frame(model_summary$Coef)
     coefs$variable = names(model_summary$fit$coefficients)
     coefs$formula = model_name
     coefs$neighbours = Amat_name
-    
+
     # Creating a summary of model fit
-    model_ranking = list(model_name, 
+    model_ranking = list(model_name,
                          Amat_name,
-                         model_summary$LL, 
-                         car_output$fit$s2, 
+                         model_summary$LL,
+                         car_output$fit$s2,
                          2 * model_summary$parameters - (2*model_summary$LL))
-    
+
     # Saving model files
     print("Saving Files")
-    
+
     write.csv(coefs, filename)
     write.table(model_ranking, "results/regression_results/model_ranking.csv",
                 sep=",", append=TRUE, col.names = FALSE)
-    
+
     print(paste("Finished Investigation for: ", model_name, Amat_name))
   }
- 
+
 }
 
 
@@ -350,8 +354,8 @@ for (n_model in 1:(length(models))){
 final_model <- model_9
 final_matrix <- A.rook_shared_edge
 
-car_output <- spautolm(formula = final_model, 
-                       data = LDN_grid, listw=final_matrix, 
+car_output <- spautolm(formula = final_model,
+                       data = LDN_grid, listw=final_matrix,
                        family="CAR",
                        method = "Matrix_J")
 
@@ -383,11 +387,11 @@ print(paste("CAR Moran's I Statistic p value:",
             CAR_residual_moranI$p.value))
 
 print(paste("Difference Moran's I Statistic:",
-            linear_residual_moranI$statistic - 
+            linear_residual_moranI$statistic -
               CAR_residual_moranI$statistic))
 
 print(paste("Percentage Change:",
-            round((CAR_residual_moranI$statistic - 
+            round((CAR_residual_moranI$statistic -
               linear_residual_moranI$statistic) /
               linear_residual_moranI$statistic, 4) * 100, "%"))
 
@@ -399,7 +403,7 @@ rownames(coef_cor) <- clean_coef_names(rownames(coef_cor), abbreviations = TRUE)
 colnames(coef_cor) <- clean_coef_names(colnames(coef_cor), abbreviations = TRUE)
 
 
-coef_cor <- melt(coef_cor) 
+coef_cor <- melt(coef_cor)
 ggplot(coef_cor, aes(X1, X2)) +
   geom_tile(aes(fill = value)) +
   scale_fill_gradient2(low = "red", high = "darkgreen", mid="white", midpoint=0, limits=c(-1,1)) +
@@ -415,12 +419,12 @@ plot_descriptive_ldn("signal_stochastic", "Spatial Demand", "Signal Stochastic",
                      LDN_grid)
 
 
-# Attaching fitted values 
+# Attaching fitted values
 LDN_grid$AED_demand <- car_output$fit$fitted.values
 
 
-plot_descriptive_ldn("AED_demand",title = "AED Demand", 
-                     legend_title = "Number of AEDs", map = LDN_grid, 
+plot_descriptive_ldn("AED_demand",title = "AED Demand",
+                     legend_title = "Number of AEDs", map = LDN_grid,
                      cap=TRUE, max_val = 3, bins=5)
 plotRegresssion("AED_demand",title = "AED Demand", map = LDN_grid,
                 max_relevant_val = 6)
@@ -429,4 +433,4 @@ plotRegresssion("AED_demand",title = "AED Demand", map = LDN_grid,
 # Saving Model ------------------------------------------------------------
 
 write_sf(LDN_grid, "data/Regressed_data_ldn_2026_08_06.gpkg")
-#gpkg file allows for more than 10 character file names 
+#gpkg file allows for more than 10 character file names
