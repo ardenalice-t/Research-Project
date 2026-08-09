9dd2b84972bd3937715ca880ea11629a81fc2107
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-08-09 21:19:59 +0100

    script for importing and analysing LP solutions

--- scrips/8_analysing_LP_solution.R
+++ scrips/8_analysing_LP_solution.R
@@ -2,75 +2,110 @@
 
 ## Packages ----------------------------------------------------------------
 
-
+library(sf)
+library(readr)
+library(ggplot2)
 
 ## Functions ---------------------------------------------------------------
+
+import_solution = function(filename, value_map = LDN_grid){
+  print("--- Importing Solution ---")
+  Nlocations <- nrow(LDN_grid)
+
+  # reading csv
+  solution <- read_csv(filename, col_names = FALSE, show_col_types = FALSE)
+
+  # just getting number of placed AEDs
+  solution_facsol = as.list(solution[1:Nlocations,])$X1
+  print(paste("Number of AEDs Placed: ", sum(solution_facsol)))
+
+  # Calculating remaining demand
+  initial_demand <- value_map$AED_demand.capped
+  remaining_demand <- solution[(Nlocations + 1):(2 *Nlocations),]$X1
+  remaining_demand2 <- initial_demand - solution[1:Nlocations,]$X1
+  remaining_demand2 = pmax(remaining_demand2, 0)
+  sanity_check =( max(abs(remaining_demand - remaining_demand2)) < 1e-10)
+
+  # Checking calculations have worked correctly
+  print(paste("Sanity Check: ", sanity_check))
+  if(sanity_check == FALSE){
+    print(paste("Sum demand: ", sum(initial_demand)))
+    print(paste("Sum end of solution: ", sum(remaining_demand)))
+    print(paste("Sum subtracting solutions: ", sum(remaining_demand2)))
+  }
+
+  # Calculating % demand covered
+  pc_demand_covered = (sum(initial_demand) - sum(remaining_demand2)) / sum(initial_demand) * 100
+  print(paste("Demand covered: ", round(pc_demand_covered, digits = 2), "%"))
+
+  # Returning full solution
+  return (solution$X1)
+}
+
+
 solution_to_plot = function(facility_solution, facility_object, map, demand=TRUE,
                             num_bins=6, max_relevant_val=3, legend_title="demand"){
-  chosen_1facilities = facility_object$geometry[facility_solution==1]
-  chosen_2facilities = facility_object$geometry[facility_solution==2]
-  chosen_3facilities = facility_object$geometry[facility_solution==3]
+  facility_object$num_placed <- round(facility_solution)
+  facility_object <- facility_object[(facility_object$num_placed  > 0 ),]
   if (demand==TRUE){
-    ggplot() + 
-      geom_sf(data = ldn_boundary_map) +
-      geom_sf(data = map, lwd=0.0001, 
-              aes(fill = F1_scld)) +
+    ggplot() +
+      geom_sf(data = LDN_boundary) +
+      geom_sf(data = map, lwd=0.0001,
+              aes(fill = AED_demand.capped)) +
       scale_fill_steps(breaks = seq(0, max_relevant_val, length = num_bins),
                        limit = c(0,10000),
                        na.value = "light blue",
-                       rescaler = ~ scales::rescale_max(.x, from =c(0,max_relevant_val)), 
-                       name = legend_title) + 
-      geom_sf(data=chosen_1facilities,
+                       rescaler = ~ scales::rescale_max(.x, from =c(0,max_relevant_val)),
+                       name = legend_title) +
+      geom_sf(data=facility_object,
               size = 0.0001,alpha = 0.5,
-              colour="yellow") +
-      geom_sf(data=chosen_2facilities,
-              size = 0.0001,alpha = 0.5,
-              colour="orange") +
-      geom_sf(data=chosen_3facilities,
-              size = 0.0001,alpha = 0.5,
-              colour="red") +
+              aes(colour=as.character(num_placed))) +
+      scale_color_discrete(palette = c( "yellow", "orange", "red"), name = "Number of AEDs") +
       xlab("Longitude") +
       ylab("Latitude") +
-      coord_sf(crs = st_crs(ldn_boundary_map))
+      coord_sf(crs = st_crs(LDN_boundary))
   }
   else{
-    ggplot() + 
-      geom_sf(data = ldn_boundary_map) +
-      geom_sf(data=chosen_1facilities,
+    ggplot() +
+      geom_sf(data = LDN_boundary) +
+      geom_sf(data=facility_object,
               size = 0.0001,alpha = 0.5,
-              colour="yellow") +
-      geom_sf(data=chosen_2facilities,
-              size = 0.0001,alpha = 0.5,
-              colour="orange") +
-      geom_sf(data=chosen_3facilities,
-              size = 0.0001,alpha = 0.5,
-              colour="red") +
+              aes(colour=as.character(num_placed))) +
+      scale_color_discrete(palette = c( "yellow", "orange", "red"),
+                           name = "Number of AEDs") +
       xlab("Longitude") +
       ylab("Latitude") +
-      coord_sf(crs = st_crs(ldn_boundary_map))
+      coord_sf(crs = st_crs(LDN_boundary))
   }
-  
+
 }
 
-process_lp_result = function(lp_sol, map, print_sol_vec=FALSE){
-  print(lp_sol)
-  solution_vector <- lp_sol$solution
-  if(print_sol_vec == TRUE){print(cat("Solution vector ", solution_vector))}
-  
-  facility_solution <- solution_vector[1:Nlocations]
-  
-  initial_demand <- map$F1_mutated
-  remaining_demand <- solution_vector[(Nlocations + 1):(2 *Nlocations)]
-  
-  pc_demand_covered = (sum(initial_demand) - sum(remaining_demand)) / sum(initial_demand) * 100
-  print(paste("% Demand covered: ", pc_demand_covered))
-  
-  cat("Facility solution:", facility_solution)
-  return(facility_solution)
-}
 
-sf_to_latlong_matix <- function(sf_object){
-  result = as.data.frame(st_coordinates(sf_object))
-  names(result)= c("long","lat")
-  return(result)
-}
+
+## Files -------------------------------------------------------------------
+
+LDN_grid <- read_sf("data/capped_data_ldn.gpkg")
+
+# Making Boundary Map -----------------------------------------------------
+
+LDN_boundary <- st_cast(st_union(LDN_grid), "POLYGON")
+
+
+# Reading in a Solution ---------------------------------------------------
+
+solution_2062 <- import_solution("matrix_exports/gradated_sol_2062AEDs_18159locations.csv")
+
+solution_4124 <- import_solution("matrix_exports/gradated_sol_4124AEDs_18159locations.csv")
+
+solution_8248 <- import_solution("matrix_exports/gradated_sol_8248AEDs_18159locations.csv")
+
+old_solution_8248 <- read_csv("matrix_exports/solution_7479.csv",
+                              col_names = FALSE, show_col_types = FALSE)$X1
+
+solution_10_loc <- read_csv("matrix_exports/gradated_sol_3AEDs_10locations.csv",
+                            col_names = FALSE, show_col_types = FALSE)$X1
+
+solution_to_plot(facility_solution = solution_2062[1:18159],
+                 facility_object = st_centroid(LDN_grid),
+                 map = LDN_grid,
+                 demand = FALSE)
