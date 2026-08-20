9af822bc4e91728b8ad1bd3da4e015db536a3b69
Arden Temple-Bowles <aat25@ic.ac.uk>
2026-08-20 17:33:20 +0100

    removing titles from plots

--- src/plot_descriptive_ldn.R
+++ src/plot_descriptive_ldn.R
@@ -1,7 +1,6 @@
 #' plot_descriptive_fill_ldn
 #'
 #' @param relevant_col - a string of the relevant column to fill based on
-#' @param title title for the plot
 #' @param legend_title title for the legend
 #' @param map map containing the data to use
 #'
@@ -11,18 +10,15 @@
 #' @examples
 #'
 #' plot_descriptive_ldn(relevant_col = 'avg_hos_dpr',
-#' title = "Household Deprivation by LSOA",
 #' legend_title = "Average Number of \ n Deprivation Dimensions", map = LSOA_map)
 #'
-#' plot_descriptive_ldn(relevant_col = 'WD_pop_den', title = "Workday Population Density by LSOA",
+#' plot_descriptive_ldn(relevant_col = 'WD_pop_den',
 #' legend_title = "Residents per sq km", map = LSOA_map)
 #'
 #'
-plot_descriptive_ldn <- function(relevant_col,
-                                 title, legend_title,
+plot_descriptive_ldn <- function(relevant_col, legend_title,
                                  map, cap=FALSE, max_val=5, bins=6){
   stopifnot(is.character(relevant_col) &
-              is.character(title) &
               is.character(legend_title))
 
   if(cap){
@@ -35,7 +31,6 @@ plot_descriptive_ldn <- function(relevant_col,
                        rescaler = ~ scales::rescale_max(.x, from =c(0,max_val)),
                        labels = scales::label_number(0.1),
                        name = legend_title)  +
-      ggtitle(label = title) +
       xlab("Longitude") +
       ylab("Latitude")
   }
@@ -46,7 +41,6 @@ plot_descriptive_ldn <- function(relevant_col,
       scale_fill_continuous(name = legend_title,
                             labels = scales::label_number(),
                             palette = "viridis") +
-      ggtitle(label = title) +
       xlab("Longitude") +
       ylab("Latitude")
   }
